ad4db93556e6fbcdc9669096cc5f25dba059dcb6
Payton-Lee <persurejonas1202@163.com>
2022-06-23 14:14:44 +0800

    完成首页数据的开发

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -23,41 +23,43 @@
             <div v-if="username !== null">欢迎您！ {{ username }}</div>
             <div>
               <el-button v-if="username !== null" class="!bg-stone-400 !text-black" @click="logout">注销登录</el-button>
-              <el-button v-else type="primary"  class="!bg-stone-400 !text-black" @click="login">登录</el-button>
+              <el-button v-else class="!bg-stone-400 !text-black" @click="login">登录</el-button>
             </div>
           </div>
         </div>
       </el-header>
       <el-main class="!px-50">
-        <div class="w-full min-h-50 bg-stone-300 opacity-70 rounded-lg">
+        <div class="w-full min-h-50 bg-stone-300 rounded-lg">
           <router-view></router-view>
         </div>
       </el-main>
-      <el-footer class="!px-50">Footer</el-footer>
+      <el-footer class="!px-50 !h-70 bg-white opacity-70">
+        
+      </el-footer>
     </el-container>
   </div>
 </template>
 
-<script setup>
-import { reactive, ref } from "vue"
-import { useRouter } from 'vue-router'
-
-const username = ref(window.sessionStorage.getItem("username"))
-const bookList = reactive([])
-const route = useRouter()
-
-
-const login = () => {
-  route.push("/login")
-}
-
-const logout = () => {
-  window.sessionStorage.removeItem("username")
-  window.sessionStorage.removeItem("token")
-  route.push("/booklist")
-  username = null
+<script>
+export default {
+  data() {
+    return {
+      username: window.sessionStorage.getItem("username"),
+    }
+  },
+  methods: {
+    login() {
+      this.$router.push("/login")
+    },
+    logout() {
+      window.sessionStorage.removeItem("username")
+      window.sessionStorage.removeItem("token")
+      this.$router.push("/booklist")
+      this.username = null
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="css" scoped>
 </style>
--- src/components/bookList/BookList.vue
+++ src/components/bookList/BookList.vue
@@ -1,53 +1,65 @@
 <template>
   <div></div>
-  <div class="!opacity-100 p-5 flex justify-start flex-wrap">
-    <goods-box class=" mx-2 my-2"></goods-box>
-    <goods-box class=" mx-2 my-2"></goods-box>
-    <goods-box class=" mx-2 my-2"></goods-box>
-    <goods-box class=" mx-2 my-2"></goods-box>
-    <goods-box class=" mx-2 my-2"></goods-box>
-    <goods-box class=" mx-2 my-2"></goods-box>
-    <goods-box class=" mx-2 my-2"></goods-box>
-    <goods-box class=" mx-2 my-2"></goods-box>
+  <div class="p-5 flex justify-start flex-wrap">
+    <div v-for="item in booklistdata" :key="item.id" class="w-63 h-90 bg-white opacity-100 rounded-lg p-2 m-2">
+      <div class="h-50 w-full rounded-lg overflow-hidded flex justify-center">
+        <el-image class="h-40 w-40" :src="'http://localhost:9001/bookshop/api/v1/image/' + item.image"></el-image>
+      </div>
+      <div class="w-full h-30 p-4 space-y-2">
+        <div class="w-full h-8 text-gray-500 text-sm">图书名称： {{ item.bookName }}</div>
+        <div class="w-full h-4 text-gray-500 text-sm leading-4">作者： {{ item.author }}</div>
+        <div class="text-orange-500 w-full h-4 leading-4 text-sm">价格: {{ item.price }}.00 元</div>
+        <div class="flex justify-end">
+          <el-button class="!m-0 !bg-stone-400 !text-black" size="default">购买</el-button>
+        </div>
+      </div>
+    </div>
+    <el-pagination v-model:currentPage="queryVo.current" v-model:page-size="queryVo.size"
+      :page-sizes="[1, 4, 8, 12, 16]" :small="small" :disabled="disabled" :background="background"
+      layout="total, sizes, prev, pager, next, jumper" :total="total" @size-change="handleSizeChange"
+      @current-change="handleCurrentChange" class="mt-3" />
   </div>
 </template>
 
 <script>
 import { bookList } from '../../axios.js'
-import { reactive, ref } from "vue"
-import { useRouter } from 'vue-router'
-import GoodsBox from '../widget/GoodsBox.vue';
-
-components: {
-  GoodsBox
-}
-
-const queryVo = reactive({
-  current: 1,
-  size: 8,
-  queryInfo: ''
-})
-
-let bookListData = reactive([])
-let total = ref(0)
-
-const getBookList = async (query) => {
-  const res = await bookList(query)
-  if(res.status !== 200) {
-    return
-  }
-  console.log(res);
-  bookListData = res.data.records
-  total = res.data.total
-  console.log(bookListData);
-  console.log(total);
-}
 
 export default {
-  setup() {
-    getBookList(queryVo)
+  data() {
+    return {
+      booklistdata: [],
+      queryVo: {
+        current: 1,
+        size: 8,
+        queryInfo: ''
+      },
+      total: 0
+    }
+  },
+  methods: {
+    async getBookList() {
+      const res = await bookList(this.queryVo)
+      if (res.status !== 200) {
+        return
+      }
+      this.booklistdata = res.data.records
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.queryVo.size = newSize
+      this.getBookList()
+    },
+    handleCurrentChange(newCurrent) {
+      this.queryVo.current = newCurrent
+      this.getBookList()
+    }
+  },
+  created() {
+    this.getBookList()
   }
 }
+
+
 </script>
 
 <style>
